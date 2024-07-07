03a5c88c05c786c5c975834cead8447f2663de12
d0d061g <davide.blancoinni@gmail.com>
2024-07-07 15:37:25 +0200

    Add files via upload

--- scripts/Index.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(`.js-todo-button`)
-  .addEventListener(`click`, () => {
-    window.location.href = "todo.html"
-  });
-
-document.querySelector(`.js-calculator-button`)
-  .addEventListener(`click`, () => {
-    window.location.href = "calculator.html"
-  });
